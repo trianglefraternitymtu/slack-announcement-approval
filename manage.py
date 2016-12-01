ada8a15720c658cfdc51f2b4eda77b97c1218e40
Ian Wakely <raveious.irw@gmail.com>
2016-12-01 16:41:42 -0500

    Adding support for local dev environment variables.

--- manage.py
+++ manage.py
@@ -3,6 +3,15 @@ import os
 import sys
 
 if __name__ == "__main__":
+    try:
+        envFile = open('.env', 'r')
+        localVars = [v.split('=', 1) for v in envFile.read().split()]
+
+        for v in localVars:
+            os.environ.setdefault(*v)
+    except FileNotFoundError as e:
+        pass
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
     from django.core.management import execute_from_command_line
