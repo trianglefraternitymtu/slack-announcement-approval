475c5bbd2acdf5158ff0d940f8b2882ccabc93db
Ian Wakely <raveious.irw@gmail.com>
2016-12-04 08:00:55 -0500

    Access token field in the database was way to short. Increasing the length and then some.

--- website/models.py
+++ website/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Team(models.Model):
     team_id = models.CharField(editable=False, primary_key=True, max_length=30)
-    access_token = models.CharField(max_length=30, editable=False)
+    access_token = models.CharField(max_length=128, editable=False)
     post_channel = models.CharField(max_length=21, default=None)
     approval_channel = models.CharField(max_length=21, default=None)
     admin_only_approval = models.BooleanField(default=True)
