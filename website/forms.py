c187ed5915414d3d82759165b071fa90d7f242ba
Ian Wakely <raveious.irw@gmail.com>
2016-12-21 12:56:04 -0500

    attrS

--- website/forms.py
+++ website/forms.py
@@ -3,8 +3,6 @@ from slacker import Slacker
 from .models import Team
 
 class TeamSettingsForm(forms.ModelForm):
-    post_channel = forms.ChoiceField()
-    approval_channel = forms.ChoiceField()
 
     def __init__(self, *args, **kwargs):
         super(TeamSettingsForm, self).__init__(*args, **kwargs)
@@ -15,11 +13,14 @@ class TeamSettingsForm(forms.ModelForm):
         pub_ch = [(c['id'], c['name']) for c in slack.channels.list().body['channels'] if not c['is_archived']]
         users = [(u['id'], u['profile']['real_name']) for u in slack.users.list().body['members'] if not u['deleted']]
 
-        self.fields['post_channel'].widget.choices = pub_ch
-        self.fields['approval_channel'].widget.choices = pub_ch + priv_ch + users
+        self.fields['post_channel'].widget.choices=tuple(pub_ch)
+        self.fields['approval_channel'].widget.choices=tuple(pub_ch + priv_ch + users)
 
     class Meta:
         model = Team
         fields = ['post_channel', 'approval_channel', 'admin_only_approval',
                   'admin_only_edit']
-        exclude = ['access_token']
+        widgets = {
+            'post_channel':  forms.ChoiceField(attrs={}),
+            'approval_channel': forms.ChoiceField(attrs={})
+        }
