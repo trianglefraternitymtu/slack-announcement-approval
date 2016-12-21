233698e26a2d51facaebec5d303af41a17daaf30
Ian Wakely <raveious.irw@gmail.com>
2016-12-21 12:25:16 -0500

    Still trying to get this form choicefield right.

--- website/forms.py
+++ website/forms.py
@@ -13,10 +13,14 @@ class TeamSettingsForm(forms.ModelForm):
         pub_ch = [(c['id'], c['name']) for c in slack.channels.list().body['channels'] if not c['is_archived']]
         users = [(u['id'], u['profile']['real_name']) for u in slack.users.list().body['members'] if not u['deleted']]
 
-        self.fields['post_channel'].widget = forms.ChoiceField(choices=tuple(pub_ch))
-        self.fields['approval_channel'].widget = forms.ChoiceField(choices=tuple(pub_ch + priv_ch + users))
+        self.fields['post_channel'].widget.choices=tuple(pub_ch)
+        self.fields['approval_channel'].widget.choices=tuple(pub_ch + priv_ch + users)
 
     class Meta:
         model = Team
         fields = ['post_channel', 'approval_channel', 'admin_only_approval',
                   'admin_only_edit']
+        widgets = {
+            'post_channel':  forms.ChoiceField(),
+            'approval_channel': forms.ChoiceField()
+        }
